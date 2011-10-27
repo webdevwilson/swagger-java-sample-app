24480da6b643f8028442c314ba65b39f21faac24
Ayush Gupta <ayush@glugbot.com>
2011-10-27 16:35:15 +0530

    fixed a bug in UserData and added CORS policies

--- src/main/java/com/wordnik/swagger/sample/util/ApiOriginFilter.java
+++ src/main/java/com/wordnik/swagger/sample/util/ApiOriginFilter.java
@@ -27,6 +27,8 @@ public class ApiOriginFilter implements javax.servlet.Filter {
 			FilterChain chain) throws IOException, ServletException {
 		HttpServletResponse res = (HttpServletResponse) response;
 		res.addHeader("Access-Control-Allow-Origin", "*");
+		res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+		res.addHeader("Access-Control-Allow-Headers", "Content-Type");
 		chain.doFilter(request, response);
 	}
 
